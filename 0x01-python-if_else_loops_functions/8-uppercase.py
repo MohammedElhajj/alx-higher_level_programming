044f3f2b6d64a717098ef0bf3a6299ec3258ff87
MohammedElhajj <m93osman@yahoo.com>
2023-01-03 07:00:34 -0800

    Task 8. Function to print a string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Function prints a string in uppercase followed by a new line"""
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
