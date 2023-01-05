f6b192175cfe98944663056f02d5ecf5629e88bd
MohammedElhajj <m93osman@yahoo.com>
2023-01-05 10:02:13 -0800

    Task 0. Import a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pythone3
+if __name__ == "__main__":
+    # Prints the result of the addition
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
