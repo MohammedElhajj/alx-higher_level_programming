a10ddfe852f699e743e862053438717ae6e853f4
MohammedElhajj <m93osman@yahoo.com>
2023-01-05 11:49:12 -0800

    Task 5. Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    # Imports and prints the value of var a from variable_load_5.py
+    # Imports and prints the value of var a from a file
     from variable_load_5 import a
 
     print(a)
