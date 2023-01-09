16a864a4fd1e2bd6ef240b6d60e27caf2073dd74
MohammedElhajj <m93osman@yahoo.com>
2023-01-09 05:08:00 -0800

    Task 4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# A function replaces an element in a list at a specific position
+# without modifying the original list
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if (idx < 0) or (idx > len(my_list) - 1):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
