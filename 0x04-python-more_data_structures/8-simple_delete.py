b7dfb5ac7da591b209b296f05a06b4a9071777a1
MohammedElhajj <m93osman@yahoo.com>
2023-01-11 12:40:34 -0800

    Task 8. Simple delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 # A function deletes a key in a dictionary
 
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key)
+    a_dictionary.pop(key, None)
     # Using pop() method with no value to be returned if
     # a key cannot be found
     return (a_dictionary)
