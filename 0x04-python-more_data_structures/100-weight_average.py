e3b8521e41c6a4f84ff6c8408aec0e933944f376
MohammedElhajj <m93osman@yahoo.com>
2023-01-11 14:13:40 -0800

    Task 13. Weighted average!

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list: 
-        # Weighted_avg = sum(i*j)/sum(j) i: int number, j: weighting factor
-        return (sum(i * j for i, j in my_list) / sum(j for i, j in my_list))
-    return (0) # If the list is empty
+        return (sum(s * w for s, w in my_list) / sum(w for s, w in my_list))
+    return 0
