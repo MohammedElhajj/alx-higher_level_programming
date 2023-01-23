7c0c665b5c5280af648f9f21504f2ac67aac0554
MohammedElhajj <m93osman@yahoo.com>
2023-01-23 13:53:11 -0800

    Task 8. A function that executes a function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct, *args):
     import sys
     try:
         func_result = fct(*args)
+        return (func_result)
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
-    return (func_result)
