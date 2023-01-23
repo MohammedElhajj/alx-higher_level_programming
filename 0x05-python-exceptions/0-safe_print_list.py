c4f3c665c48947f7216b0dc18ea7b74c561ac144
MohammedElhajj <m93osman@yahoo.com>
2023-01-23 10:58:49 -0800

    Task 0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
     Returns:
         The real number of elements printed.
     """
-    real_num = 0 # counter
+    real_num = 0
     for idx in range(x):
         try:
             print("{}".format(my_list[idx]), end="")
