aa642223e6c540bcc3a5729127a04018a7a93ca6
MohammedElhajj <m93osman@yahoo.com>
2023-01-25 05:17:36 -0800

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """A class named Square"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize the new class Square
         Args:
         size(int): the size of the square
