4fbbf8810e3a2a3d23406997642bd53665adca1c
MohammedElhajj <m93osman@yahoo.com>
2023-01-25 03:19:15 -0800

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module to create a class named Square"""
+
+
+class Square:
+    """A class named Square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
+            size: the size of the square
+        """
+        self.__size = size
