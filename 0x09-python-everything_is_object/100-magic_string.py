2d70b6b48987f228ae58e1e09fc82a6261b9ee56
MohammedElhajj <m93osman@yahoo.com>
2023-01-31 13:26:26 -0800

    Some changes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.n = 0 if not hasattr(magic_string, "n") else magic_string.n+1
+    magic_string.n = 0 if not hasattr(magic_string, "n") else magic_string.n + 1
     return ("Holberton" + ", Holberton" * magic_string.n)
