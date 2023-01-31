2970a33b526bf89660f416174a43836dfe5e0445
MohammedElhajj <m93osman@yahoo.com>
2023-01-31 04:31:01 -0800

    Task 0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,9 +12,9 @@ def add_integer(a, b=98):
     Returns:
         The return value. a + b
     """
-    if (type(a) != int or type(a) != float):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if (type(b) != int or type(b) != float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
 
