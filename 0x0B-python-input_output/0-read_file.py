5ac070e06936f7807bf894d43a188e59154f2755
MohammedElhajj <m93osman@yahoo.com>
2023-02-07 02:23:23 -0800

    Task 0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module to define the read_file function"""
+
+
+def read_file(filename=""):
+    """Function to read a text file (UTF8)
+        and print it to stdout
+    
+    Args:
+        filename(str): the File name
+    """
+    with open(filename, encoding='utf-8') as file:
+        for line in file:
+            print(line, end="")
