6ec1065f0dd04d8d9bc292f0889d34e65a9e9bfb
MohammedElhajj <m93osman@yahoo.com>
2023-03-14 12:25:22 -0700

    Task 7. First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Insert a new row in a table
 -- Insert a new row in the table first_table
-INSERT INTO 'first_table'('id', 'name')
-VALUES(89, "Best School");
+INSERT INTO first_table(id, name)
+VALUES (89, 'Best School');
