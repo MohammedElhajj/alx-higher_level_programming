9ccf514945f0929e4a6e398fc95b0a067e842330
MohammedElhajj <m93osman@yahoo.com>
2023-04-12 16:36:23 -0700

    Task 8. The reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   let l = 0;
   let r = list.length - 1;
-  for (l = 0; l < list.length / 2; l++; r--) {
+  for (l = 0; l < list.length / 2; l++, r--) {
     [list[l], list[r]] = [list[r], list[l]];
   }
   return list;
